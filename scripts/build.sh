b1691b0ef556133bff95372be3d4a725ccd168c6
Anirban Bandyopadhyay <bandyopadhyay4u@gmail.com>
2025-11-06 06:34:25 +0530

    auto create superuser

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,3 +10,7 @@ python manage.py collectstatic --noinput
 
 # Run migrations
 python manage.py migrate
+
+echo "from django.contrib.auth.models import User; 
+User.objects.filter(username='Anirban').exists() or 
+User.objects.create_superuser('Anirban', 'bandyopadhyay4u@gmail.com', 'EduCollab#123')" | python manage.py shell
